1ec3f32646812623eddb417b0f3921e1f67ff0ec
Jeferson S <64113091+jefersonroots@users.noreply.github.com>
2022-03-08 20:14:09 -0300

    Update conexao.php

--- API/conexao.php
+++ API/conexao.php
@@ -2,10 +2,10 @@
 <?php 
 
 //CONECTAR NO BANCO
-$host1 = '110.100.50.89,1440';
-$user = 'linx';
-$senha = 'Poly@123';
-$base = 'LINX_Prod';
+$host1 = '@';
+$user = '@';
+$senha = '@';
+$base = '@';
 $con = array("Database" => "$base", "UID" => "$user", "PWD" => "$senha");
 $conn1 = sqlsrv_connect($host1, $con);
 
